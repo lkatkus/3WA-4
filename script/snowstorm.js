738edd6d7e077bed2ba4e64896886454b1de6286
lkatkus <laimonas@katkus.eu>
2017-12-22 13:47:44 +0200

    Hover effect on icon. Adjusted speed of flakes.

--- script/snowstorm.js
+++ script/snowstorm.js
@@ -40,8 +40,8 @@ var snowStorm = (function(window, document) {
   this.flakeRightOffset = 0;      // Right margin/gutter space on edge of container
   this.flakeWidth = 8;            // Max pixel width reserved for snow element
   this.flakeHeight = 8;           // Max pixel height reserved for snow element
-  this.vMaxX = 5;                 // Maximum X velocity range for snow
-  this.vMaxY = 8;                 // Maximum Y velocity range for snow
+  this.vMaxX = 10;                 // Maximum X velocity range for snow
+  this.vMaxY = 10;                 // Maximum Y velocity range for snow
   this.zIndex = 0;                // CSS stacking order applied to each snowflake
 
   // --- "No user-serviceable parts inside" past this point, yadda yadda ---
